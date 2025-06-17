6b5493dfc8e59d0ba06039a342180c4170148ee2
hiromia006 <hiromia006@gmail.com>
2025-06-17 22:09:18 +0600

    TestNG 2nd class

--- src/test/java/testngClass2/MethodAnnotationTest.java
+++ src/test/java/testngClass2/MethodAnnotationTest.java
@@ -1,65 +1,37 @@
-package testngClass1;
+package testngClass2;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class FirstTestCase {
-
-    @Test(priority = 0, enabled = false)
-    public void testDoubleClick() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        Thread.sleep(4000);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
+public class MethodAnnotationTest extends BaseMethodTest {
+    @Test(priority = 0)
+    public void testDoubleClick() {
         // Navigate to the practice page for buttons
         driver.get("https://www.tutorialspoint.com/selenium/practice/buttons.php");
 
         new Actions(driver)
                 .doubleClick(driver.findElement(By.cssSelector(".btn.btn-success")))
                 .perform();
-
         String txt = driver.findElement(By.id("doublec")).getText().trim();
         Assert.assertEquals(txt, "You have Double clicked");
-
-        driver.quit();
     }
 
 
-    @Test(priority = 1, enabled = false)
+    @Test(priority = 1)
     public void checkPageTittle() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        Thread.sleep(4000);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
         driver.get("https://www.tutorialspoint.com/selenium/practice/buttons.php");
-
         String title = driver.getTitle().trim();
-
         Assert.assertEquals(title, "Selenium Practice - Buttons");
 
-        driver.quit();
     }
 
     @Test(priority = 2)
     public void validatePageUrl() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        Thread.sleep(4000);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
         driver.get("https://www.tutorialspoint.com/selenium/practice/buttons.php");
-
         String url = driver.getCurrentUrl().trim();
         Assert.assertEquals(url, "https://www.tutorialspoint.com/selenium/practice/buttons.php");
 
-        driver.quit();
     }
 }
